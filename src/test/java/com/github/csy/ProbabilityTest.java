dc4c65ed10e9b7ac6d1948cae83091e4c767bc66
wanghaiqing <krman@163.com>
2019-06-27 17:42:30 +0800

    csy update test

--- src/test/java/com/github/csy/ProbabilityTest.java
+++ src/test/java/com/github/csy/ProbabilityTest.java
@@ -9,22 +9,32 @@ public class ProbabilityTest {
 
     @Test
     public void should_get_andMethod() {
+        //given
         Probability beforeAndValue = new Probability((float) 0.5);
         Probability afterAndValue = new Probability((float) 0.8);
+        //when
+
+        //then
         Assert.assertEquals( 0.4, beforeAndValue.andMethod(afterAndValue).getValue(), 0.00001);
     }
 
     @Test
     public void should_get_notMethod() {
+        //given
         Probability notValue = new Probability((float) 0.3);
+        //when
+        //then
         Assert.assertEquals(0.7, notValue.notMethod().getValue(), 0.00001);
 
     }
 
     @Test
     public void should_get_orMethod(){
+        //given
         Probability beforeOrValue = new Probability((float) 0.3);
         Probability afterOrValue = new Probability((float) 0.6);
+        //when
+        //then
         Assert.assertEquals(0.72, beforeOrValue.orMethod(afterOrValue).getValue(), 0.00001);
 
     }
